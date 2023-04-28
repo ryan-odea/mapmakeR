4f61b75515116442c1cb43b8eda826531fb791c5
Ryan O'Dea <70209371+ryan-odea@users.noreply.github.com>
2023-04-28 10:48:39 -0400

    namespace update

--- R/star_map.R
+++ R/star_map.R
@@ -8,7 +8,7 @@
 #' @importFrom osmdata getbb
 #' @importFrom ggplot2 ggplot theme_void theme labs geom_sf element_text margin element_rect element_line annotation_custom
 #' @importFrom dplyr mutate filter
-#' @importFrom sf st_read st_crs st_transform  st_intersection st_buffer st_sfc
+#' @importFrom sf st_read st_crs st_transform  st_intersection st_buffer st_sfc st_point
 #' @importFrom lubridate yday
 #'
 #' @export
